8792d959699eeb5c746425fcb6ca213579a16d16
Vishagen Moonesawmy <vmoonesa@uwaterloo.ca>
2022-12-20 17:21:44 -0500

    API Endpoint to Create Employee

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const db = mysql.createConnection({
     database: "mytable"
 })
 
+app.use(express.json())
+
 app.get("/employees", (req, res) => {
     const q = "SELECT * FROM employee";
     db.query(q, (err, data) => {
@@ -18,6 +20,23 @@ app.get("/employees", (req, res) => {
             return res.json(err);
         }
         return res.json(data);
+    });
+});
+
+app.post("/employee", (req, res) => {
+    const q = "INSERT INTO employee(`firstName`, `lastName`, `salary`) VALUES (?)";
+  
+    const values = [
+      req.body.firstName,
+      req.body.lastName,
+      req.body.salary,
+    ];
+  
+    db.query(q, [values], (err, data) => {
+      if (err){
+        return res.send(err);
+      } 
+      return res.json("Employee Successfully Created");
     });
 });
 
